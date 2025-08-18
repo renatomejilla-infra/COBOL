913297b6c342b71c110c74ed267f79e7c7161c1f
renatomejilla-infra <renatomejilla.infra@gmail.com>
2025-08-18 15:22:20 -0700

    A demo of calling a program

---
+++ Sample Codes/EX003.cob
@@ -0,0 +1,13 @@
+       identification division.
+       program-id. ex003.
+
+       environment division.
+       data division.
+       
+       procedure division.
+           display "I am now in the other program." line 8 col 1.
+           display "Let's go back to main." line 9 col 1.
+           goback.
+
+           stop run.
+
