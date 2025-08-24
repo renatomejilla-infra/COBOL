240ca774debbe3df777f3d06b40fbe2cc8723468
renatomejilla-infra <renatomejilla.infra@gmail.com>
2025-08-24 18:43:41 +0800

    A sample of UPPER to LOWER string conversion

--- Sample Codes/EX004.cob
+++ Sample Codes/EX004.cob
@@ -1,20 +1,19 @@
       *    Author     : Renato D. Mejilla
       *    Date       : August 11, 2025
-      *    Description: A demo of converting upper-case words into 
-      *                  lower-case.
-      *    Tested     : Using GNUCobol
-      
+      *    Description: A demo of converting upper-case words into lower-case.
+      *    Compiler   : Micro Focus Visual Cobol
+      *    ===================================================================
        identification division.
-       program-id. EX004.
+       program-id.      uppertolower.
 
        environment division.
        data division.
        working-storage section.
-       01  ws-user-input   pic x(20).
-       01  ws-converted    pic x(20).
+       01  ws-user-input   pic x(100).
+       01  ws-converted    pic x(100).
 
        procedure division.
-           display "Enter an upper-case letter: " with no advancing.
+           display "Enter an upper-case words: " with no advancing.
            accept ws-user-input.
 
            move ws-user-input to ws-converted.
